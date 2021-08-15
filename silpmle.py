2389674496001c83a15e7f907bdb71c7658e0799
anigani1998 <88969247+anigani1998@users.noreply.github.com>
2021-08-15 17:39:01 +0530

    Add files via upload

---
+++ silpmle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 15 17:14:06 2021
+
+@author: User
+"""
+
+list1 =[1, "hellow", "Word", True]
+
+list2 = list(range(10))
+print(list1)
+print(list2)
+print(list1[-2])
+print(list(2))
